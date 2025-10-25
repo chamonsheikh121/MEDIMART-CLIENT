c1571095ef2dfd86e7e32b951c50085a4641a907
Chamon ali <studentchamon1212@gmail.com>
2025-10-25 23:41:45 +0600

    initialized medimart

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'azim.hostlin.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
